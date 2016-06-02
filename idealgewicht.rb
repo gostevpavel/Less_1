1f546a2753fc82d4a0c7eb1a503d4ab5f83346a5
pvg <gostevpavel@gmail.com>
2016-06-02 15:58:22 +0200

    программа готова

---
+++ idealgewicht.rb
@@ -0,0 +1,7 @@
+puts "Привет! Напиши свое имя."
+name=gets.chomp
+puts "Какой твой рост?"
+height = gets.chomp
+weight = height.to_i-110
+puts "#{name}, твой идеальный вес #{height.to_i-110} кг" if weight>0
+puts"Ваш вес уже оптимальный" if weight.to_i<0
